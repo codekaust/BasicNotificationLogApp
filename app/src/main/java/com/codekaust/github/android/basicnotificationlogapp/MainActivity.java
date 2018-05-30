538189ba3cc3e670b8524838cf83a638886712ba
codekaust <kaustubh1303@gmail.com>
2018-05-30 22:57:39 +0530

    added refresh button

--- app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/MainActivity.java
+++ app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -73,5 +74,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         realm.close();
+    }
+
+
+    public void updateArrayList(View view) {
+        updateArrayList();
     }
 }
--- app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/NotificationListener_Service.java
+++ app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/NotificationListener_Service.java
@@ -81,4 +81,5 @@ public class NotificationListener_Service extends NotificationListenerService {
         });
 
     }
+    
 }
