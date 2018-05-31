5551241d47320a02df25989003146ba0bbbfe824
codekaust <kaustubh1303@gmail.com>
2018-05-31 07:48:18 +0530

    updated refresh and added notifCreate btn

--- app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/MainActivity.java
+++ app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/MainActivity.java
@@ -2,16 +2,20 @@ package com.codekaust.github.android.basicnotificationlogapp;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.nfc.Tag;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -30,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ArrayAdapter<NotificStorageRealm> arrayAdapter;
 
-    public static Activity mainActy;
+    private Button createNotif, refreshList;
+
+    public static boolean isNotificationAccessEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,46 +44,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        listNotifs=new ArrayList<NotificStorageRealm>();
+        listNotifs = new ArrayList<NotificStorageRealm>();
 
         realm = Realm.getDefaultInstance();
 
-        Toast.makeText(this, "Please give Notification Access Permission to \nNotificationListener_Service\n if not given...", Toast.LENGTH_LONG).show();
+        if (!isNotificationAccessEnabled) {
+            Toast.makeText(this, "Please give Notification Access Permission to \nNotificationListener_Service\n if not given...", Toast.LENGTH_LONG).show();
+            startActivity(new android.content.Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+        }
 
-        startActivity(new android.content.Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+        listView = (ListView) findViewById(R.id.listView);
 
-        updateArrayList();
-
-        listView=(ListView)findViewById(R.id.listView);
-
-        arrayAdapter = new ArrayAdapter<NotificStorageRealm>( this,
+        arrayAdapter = new ArrayAdapter<NotificStorageRealm>(this,
                 android.R.layout.simple_list_item_1,
                 listNotifs);
 
         listView.setAdapter(arrayAdapter);
+
+        createNotif = (Button) findViewById(R.id.btn_createNotif);
+        createNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotification(getApplicationContext());
+            }
+        });
+
+        refreshList = (Button) findViewById(R.id.btn_refreshList);
+        refreshList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateArrayList();
+            }
+        });
     }
 
-    public void updateArrayList(){
-
+    public void updateArrayList() {
         RealmResults<NotificStorageRealm> RRobjectNotificStorageRealms = realm.where(NotificStorageRealm.class).findAll();
 
         realm.beginTransaction();
-        for (NotificStorageRealm notificStorageRealm : RRobjectNotificStorageRealms){
+        for (NotificStorageRealm notificStorageRealm : RRobjectNotificStorageRealms) {
             listNotifs.add(notificStorageRealm);
         }
         realm.commitTransaction();
 
+        arrayAdapter.notifyDataSetChanged();
+    }
 
+    private void createNotification(Context context) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setContentTitle("My Notification");
+        builder.setContentText("Notification created.");
+        builder.setTicker("Notification created.");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setAutoCancel(true);
+        notificationManager.notify((int) System.currentTimeMillis(), builder.build());
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         realm.close();
-    }
-
-
-    public void updateArrayList(View view) {
-        updateArrayList();
     }
 }
--- app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/NotificationListener_Service.java
+++ app/src/main/java/com/codekaust/github/android/basicnotificationlogapp/NotificationListener_Service.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 import android.os.Bundle;
 import android.graphics.Bitmap;
 
+import java.time.chrono.MinguoChronology;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -20,32 +22,40 @@ public class NotificationListener_Service extends NotificationListenerService {
 
     @Override
     public IBinder onBind(Intent intent) {
-
-        return super.onBind(intent);
+        IBinder mIBinder = super.onBind(intent);
+        MainActivity.isNotificationAccessEnabled = true;
+        return mIBinder;
     }
 
 
     @Override
-    public void onNotificationPosted(StatusBarNotification sbn) {
-        realm=Realm.getDefaultInstance();
+    public boolean onUnbind(Intent intent) {
+        boolean mOnUnbind = super.onUnbind(intent);
+        MainActivity.isNotificationAccessEnabled = false;
+        return mOnUnbind;
+    }
 
-        Bundle extras=sbn.getNotification().extras;
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        realm = Realm.getDefaultInstance();
+
+        Bundle extras = sbn.getNotification().extras;
 
         String notificationTitle = extras.getString(android.app.Notification.EXTRA_TITLE);
 //        int notificationIcon = extras.getInt(android.app.Notification.EXTRA_SMALL_ICON);
 //        Bitmap notificationLargeIcon =
 //                     ((android.graphics.Bitmap) extras.getParcelable(android.app.Notification.EXTRA_LARGE_ICON));
         String notificationText = null;
-        if(extras.getCharSequence(android.app.Notification.EXTRA_TEXT)!=null){
-            notificationText=extras.getCharSequence(android.app.Notification.EXTRA_TEXT).toString();
+        if (extras.getCharSequence(android.app.Notification.EXTRA_TEXT) != null) {
+            notificationText = extras.getCharSequence(android.app.Notification.EXTRA_TEXT).toString();
         }
         String notificationSubText = null;
-        if(extras.getCharSequence(android.app.Notification.EXTRA_SUB_TEXT)!=null){
-            notificationSubText=extras.getCharSequence(android.app.Notification.EXTRA_SUB_TEXT).toString();
+        if (extras.getCharSequence(android.app.Notification.EXTRA_SUB_TEXT) != null) {
+            notificationSubText = extras.getCharSequence(android.app.Notification.EXTRA_SUB_TEXT).toString();
         }
         String package_name = sbn.getPackageName();
 
-        updateDB(package_name,notificationTitle,notificationText,notificationSubText);
+        updateDB(package_name, notificationTitle, notificationText, notificationSubText);
     }
 
     @Override
@@ -53,7 +63,7 @@ public class NotificationListener_Service extends NotificationListenerService {
         Toast.makeText(this, "RemovedNotif _ BasicNotifAPP", Toast.LENGTH_SHORT).show();
     }
 
-    public void updateDB(final String pack_name, final String title, final String text, final String subText){
+    public void updateDB(final String pack_name, final String title, final String text, final String subText) {
 
         realm.executeTransactionAsync(new Realm.Transaction() {
             @Override
@@ -68,7 +78,7 @@ public class NotificationListener_Service extends NotificationListenerService {
             @Override
             public void onSuccess() {
                 // Transaction was a success.
-                Log.v("NotifService","DB_Inserted_Success");
+                Log.v("NotifService", "DB_Inserted_Success");
 
                 Toast.makeText(NotificationListener_Service.this, "Written in DB", Toast.LENGTH_SHORT).show();
             }
@@ -76,10 +86,10 @@ public class NotificationListener_Service extends NotificationListenerService {
             @Override
             public void onError(Throwable error) {
                 // Transaction failed and was automatically canceled.
-                Log.v("NotifService","DB_Inserted_FAILED");
+                Log.v("NotifService", "DB_Inserted_FAILED");
             }
         });
 
     }
-    
+
 }
